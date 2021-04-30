f177e545ce1622043253a5f6c0536a8a543b4a7a
Howard Huang <howardhuang@fb.com>
2021-04-30 07:32:12 -0700

    split up single process execution

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from agent import MarioAgent
 from metric_logger import MetricLogger
 import argparse
 import threading
+from execution_mode.single_process import single_process_exec
 
 LEARNER_NAME = "learner"
 ACTOR_NAME = "actor{}"
@@ -132,26 +133,6 @@ class Actor:
 
         return end_episode
 
-parser = argparse.ArgumentParser(
-    description="RPC Reinforcement Learning for Mario",
-    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-)
-
-parser.add_argument(
-    "--world_size", default=5, type=int, metavar="W", help="number of workers"
-)
-parser.add_argument(
-    "--num_episodes", default=100, type=int, metavar="N", help="number of episodes"
-)
-parser.add_argument(
-    "--log_interval",
-    type=int,
-    default=10,
-    metavar="N",
-    help="episode interval between logs",
-)
-args = parser.parse_args()
-
 
 def run_worker(rank, world_size, num_episodes, log_interval):
     os.environ["MASTER_ADDR"] = "localhost"
@@ -176,64 +157,26 @@ def multi_process():
     mp.spawn(run_worker, args=(args.world_size, args.num_episodes, args.log_interval), nprocs=args.world_size, join=True)
 
 
-# Single process implementation for reference and testing
-def single_process():
-    args = parser.parse_args()
-    env = create_env("SuperMarioBros-1-1-v0")
-
-    use_cuda = torch.cuda.is_available()
-    print(f"Using CUDA: {use_cuda}")
-    print()
-
-    save_dir = Path("checkpoints") / datetime.datetime.now().strftime(
-        "%Y-%m-%dT%H-%M-%S"
-    )
-    save_dir.mkdir(parents=True)
-
-    mario = MarioAgent(
-        state_dim=(4, 84, 84), action_dim=env.action_space.n, save_dir=save_dir
-    )
-
-    logger = MetricLogger(save_dir)
-
-    episodes = 51
-    for e in range(episodes):
-
-        state = env.reset()
-
-        # Play the game!
-        while True:
-
-            # Run agent on the state
-            action = mario.act(state)
-
-            # Agent performs action
-            next_state, reward, done, info = env.step(action)
-
-            # Remember
-            mario.cache(state, next_state, action, reward, done)
-
-            # Learn
-            q, loss = mario.learn()
-
-            # Logging
-            logger.log_step(reward, loss, q)
-
-            # Update state
-            state = next_state
-
-            # Check if end of game
-            if done or info["flag_get"]:
-                break
-
-        logger.log_episode()
-
-        if e % args.log_interval == 0:
-            logger.record(
-                episode=e, epsilon=mario.exploration_rate, step=mario.curr_step
-            )
-
-
 if __name__ == "__main__":
-    # single_process()
-    multi_process()
+    parser = argparse.ArgumentParser(
+        description="RPC Reinforcement Learning for Mario",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    parser.add_argument(
+        "--world_size", default=5, type=int, metavar="W", help="number of workers"
+    )
+    parser.add_argument(
+        "--num_episodes", default=100, type=int, metavar="N", help="number of episodes"
+    )
+    parser.add_argument(
+        "--log_interval",
+        type=int,
+        default=10,
+        metavar="N",
+        help="episode interval between logs",
+    )
+    args = parser.parse_args()
+
+    single_process_exec(args)
+    # multi_process()
